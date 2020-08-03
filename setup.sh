cba619f6b9c664a42322d69b5c0dc1c13b3ec76a
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-08-04 00:18:49 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 sudo apt-get update
 sudo apt-get install -y git
 
-# bat コマンドをインストール
+# Install bat
 curl -LJO https://github.com/sharkdp/bat/releases/download/v0.9.0/bat_0.9.0_amd64.deb
 sudo dpkg -i bat_0.9.0_amd64.deb
+rm bat_0.9.0_amd64.deb
+
+# Install xclip
+sudo apt-get install -y xclip
