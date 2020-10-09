28f9f5bff5f2ab286037d740e4908971fd909b4f
dilmnqvovpnmlib <44946173+dilmnqvovpnmlib@users.noreply.github.com>
2020-10-09 15:19:44 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
+
+# Install git
 sudo apt-get install -y git
 
 # Install bat
@@ -8,3 +10,6 @@ rm bat_0.9.0_amd64.deb
 
 # Install xclip
 sudo apt-get install -y xclip
+
+# Install make
+sudo apt-get install -y build-essential
